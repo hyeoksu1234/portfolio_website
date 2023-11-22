76e821d91eebd265b7bf6f4ca0fd4e1395e0eae6
hyeoksu1234 <114197095+hyeoksu1234@users.noreply.github.com>
2023-11-22 17:48:43 +0900

    1122_05:48

--- sketch2.js
+++ sketch2.js
@@ -1,34 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const photo = document.querySelector(".photo");
-
-  photo.addEventListener("click", function () {
-    if (window.innerWidth <= 1000) {
-      // 모바일 화면 크기 기준 (1000px 이하)
-      if (photo.style.transform === "scale(1.22)") {
-        photo.style.transform = "scale(1)"; // 원래 크기로 되돌리기
-      } else {
-        photo.style.transform = "scale(1.22)"; // 이미지 크기를 1.5배로 확대
-      }
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const photo = document.querySelector(".projects_photo");
-
-  photo.addEventListener("click", function () {
-    if (window.innerWidth <= 1000) {
-      // 모바일 화면 크기 기준 (1000px 이하)
-      if (photo.style.transform === "scale(1.22)") {
-        photo.style.transform = "scale(1)"; // 원래 크기로 되돌리기
-      } else {
-        photo.style.transform = "scale(1.22)"; // 이미지 크기를 1.5배로 확대
-      }
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
   const clickableLinks = document.querySelectorAll(".clickable");
 
   clickableLinks.forEach((link) => {
